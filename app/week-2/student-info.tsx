258b5847bc5408c271b7b754053526dfac20216d
Henry Leung <henry.leung@edu.sait.ca>
2026-01-20 14:19:32 -0700

    Push to Github Repository
    Pushes the local repository into Github.

---
+++ app/week-2/student-info.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <nav>
+            <h1>Henry Leung</h1>
+            <p>Github: <Link href="https://github.com/Edu-Henry-Leung">Link Here</Link></p>
+        </nav>
+    );
+}
