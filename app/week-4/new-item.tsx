e529630fc90fbac388c9f5e3664f8f8cc2b2c958
Henry Leung <henry.leung@edu.sait.ca>
2026-02-04 13:40:53 -0700

    Adding Further functionality to form from week 4

--- app/week-4/new-item.tsx
+++ app/week-4/new-item.tsx
@@ -29,6 +29,11 @@ export default function AddItems() {
 
     const handleSubmit = (event: React.FormEvent) => {
         event.preventDefault();
+
+        if (name.length < 2) {
+            alert("The Item name must have at least 2 characters!");
+            return;
+        }
         
         const newItem = {
             name: name,
@@ -43,46 +48,54 @@ export default function AddItems() {
             "Category: " + category + "\n"
         );
 
-        name = "";
-        quantity = 1;
-        category = "Produce";
+        setName("");
+        setQuantity(1);
+        setCategory("Produce");
+        setNameTouched(false);
 
     }
 
     return (
-        <div className="flex w-1/2 justify-center">
-            <form className="flex bg-white rounded p-6" onSubmit={handleSubmit}>
-                <div className="px-3">
-                    <label className="block text-black">Item Name: </label>
-                    <input className={'rounded border border-black bg-gray-100 text-black'} type="text" name="name" onBlur={handleNameTouched} value={name} onChange={handleNameChange} required/>
-                </div>
-                
-                <div className="px-3">
-                    <label className="block text-black">Quantity: </label>
-                    <input className="rounded border border-black bg-gray-100 text-black" aria-valuemin={1} aria-valuemax={99} type="number" name="quantity" value={quantity} onChange={handleQuantityChange} required/>
-                </div>
-                
-                <div className="px-3">
-                    <label className="block text-black">Category:</label>
-                    <select className="text-black" name="category" onChange={handleCategoryChange}>
-                        <option value={category}>Produce</option>
-                        <option value="Dairy">Dairy</option>
-                        <option value="Bakery">Bakery</option>
-                        <option value="Meat">Meat</option>
-                        <option value="Frozen Foods">Frozen Foods</option>
-                        <option value="Canned Goods">Canned Goods</option>
-                        <option value="Dry Goods">Dry Goods</option>
-                        <option value="Beverages">Beverages</option>
-                        <option value="Snacks">Snacks</option>
-                        <option value="Household">Household</option>
-                        <option value="Other">Other</option>
-                    </select>
-                </div>
-                
-                <button className="rounded p-2 bg-gray-200 text-black font-bold"type="submit" value="Add Item">Add Item</button>
+        <div className="flex justify-center">
+            <div className="flex rounded w-1/2 m-5 bg-white justify-center">
+                <form className="p-6" onSubmit={handleSubmit}>
+                    <div className="px-3 mb-4">
+                        <label className="block text-black ">Item Name: </label>
+                        <input className={nameTouched && name === ""? 'rounded border border-red-500 bg-gray-100 text-black'
+                            :'rounded border border-black bg-gray-100 text-black'} type="text" name="name" onBlur={handleNameTouched} value={name} onChange={handleNameChange} required/>
+                        <p className="text-red-500">{nameTouched && name === "" && 'Item Name is empty!'}</p>
+                    </div>
+                    
+                    <div className="px-3 mb-4">
+                        <label className="block text-black">Quantity: </label>
+                        <input className="rounded border border-black bg-gray-100 text-black" aria-valuemin={1} aria-valuemax={99} type="number" name="quantity" value={quantity} onChange={handleQuantityChange} required/>
+                    </div>
+                    
+                    <div className="px-3 mb-8">
+                        <label className="block text-black">Category:</label>
+                        <select className="text-black" name="category" onChange={handleCategoryChange}>
+                            <option value={category}>Produce</option>
+                            <option value="Dairy">Dairy</option>
+                            <option value="Bakery">Bakery</option>
+                            <option value="Meat">Meat</option>
+                            <option value="Frozen Foods">Frozen Foods</option>
+                            <option value="Canned Goods">Canned Goods</option>
+                            <option value="Dry Goods">Dry Goods</option>
+                            <option value="Beverages">Beverages</option>
+                            <option value="Snacks">Snacks</option>
+                            <option value="Household">Household</option>
+                            <option value="Other">Other</option>
+                        </select>
+                    </div>
+                    
+                    <div className="flex justify-center">
+                        <button className="rounded p-2 bg-sky-300 disabled:bg-gray-200 disabled:cursor-not-allowed disabled:text-gray-400 text-black font-bold" disabled={name === ""}type="submit" value="Add Item">Add Item</button>
+                    </div>
 
-            </form>
+                </form>
+            </div>
         </div>
+        
         
     )
 }
